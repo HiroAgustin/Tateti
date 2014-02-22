443cb4c494a70bf9ef6bf9abb0177522d41a9b26
HiroAgustin <hiroagustin@gmail.com>
2014-02-22 15:22:59 -0200

    missing game over validation, ui done

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function (grunt)
+{
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        watch: {
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                }
+            ,   files: ['index.html']
+            }
+        }
+    ,   connect: {
+            options: {
+                port: 9000
+            ,   livereload: 35729
+            ,   hostname: '0.0.0.0'
+            }
+        ,   livereload: {
+                options: {
+                    open: true
+                // ,   base: ['../']
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['connect:livereload', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Tic Tac Toe
+
+Simple web based tic tac toe.
+
+## Hacker School Me!
