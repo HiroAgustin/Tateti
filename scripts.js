9fab0d16f6db659daceb0edf3184472b1d4a1ad5
HiroAgustin <hiroagustin@gmail.com>
2014-02-25 23:30:04 -0200

    add rows and columns data to tiles, validate if three in line player won

--- scripts.js
+++ scripts.js
@@ -1,17 +1,46 @@
 (function (win, doc, undefined)
 {
+    function forEach(list, iterator)
+    {
+        return Array.prototype.forEach.call(list, iterator);
+    }
+
     var game = {
-            board: doc.getElementById('js-board')
-        ,   marker: doc.getElementById('js-marker')
-        ,   indicator: doc.getElementById('js-player-indicator')
-        ,   playerOne: true
-        ,   init: function ()
+
+            init: function ()
+            {
+                this.isPlayerOne = true;
+                return this.mapDom().tagTiles().addListeners();
+            }
+
+        ,   mapDom: function ()
+            {
+                this.board = doc.getElementById('js-board')
+                this.marker = doc.getElementById('js-marker')
+                this.indicator = doc.getElementById('js-player-indicator')
+
+                return this;
+            }
+
+        ,   tagTiles: function ()
+            {
+                forEach(this.board.children, function (element, index)
+                {
+                    element.dataset.row = parseInt(index / 3);
+                    element.dataset.column = index % 3;
+                });
+
+                return this;
+            }
+
+        ,   addListeners: function ()
             {
                 var self = this;
 
                 this.board.addEventListener('click', function (e)
                 {
-                    !self.isSelected(e.target) && self.select(e.target);
+                    if (!self.winner && !self.isSelected(e.target))
+                        self.select(e.target);
                 });
 
                 return this;
@@ -22,40 +51,9 @@
                 return element.classList.contains('selected');
             }
 
-        ,   isGameOver: function ()
-            {
-                var board = this.board
-                ,   tiles = board.children;
-
-
-                if (!board.querySelectorAll(':not(.selected)').length)
-                    return true;
-
-                return false;
-            }
-
         ,   getPlayerId: function ()
             {
-                return this.playerOne ? '1' : '2';
-            }
-
-        ,   togglePlayer: function ()
-            {
-                this.playerOne = !this.playerOne;
-                this.indicator.innerHTML = this.getPlayerId();
-                this.indicator.classList.toggle('player-2');
-
-                return this;
-            }
-
-        ,   celebrate: function ()
-            {
-                if (this.winner)
-                    return;
-                else
-                    this.marker.innerHTML = 'It\'s a tie <b>☹</b>';
-
-                return this;
+                return this.isPlayerOne ? '1' : '2';
             }
 
         ,   select: function (element)
@@ -67,6 +65,58 @@
                     this.togglePlayer();
                 else
                     this.celebrate();
+
+                return this;
+            }
+
+        ,   togglePlayer: function ()
+            {
+                this.isPlayerOne = !this.isPlayerOne;
+                this.indicator.innerHTML = this.getPlayerId();
+                this.indicator.classList.toggle('player-2');
+
+                return this;
+            }
+
+        ,   isGameOver: function ()
+            {
+                if (this.hasPlayerWon())
+                    return (this.winner = this.getPlayerId());
+                else if (!this.board.querySelectorAll(':not(.selected)').length)
+                    return true;
+
+                return false;
+            }
+
+        ,   hasPlayerWon: function ()
+            {
+                var tiles = this.board.getElementsByClassName('player-' + this.getPlayerId());
+
+                return tiles.length >= 3 && (this.haveThree(tiles, 'row') || this.haveThree(tiles, 'column'));
+            }
+
+        ,   haveThree: function (tiles, data)
+            {
+                var valid = false
+                ,   grouped = {};
+
+                forEach(tiles, function (element)
+                {
+                    grouped[element.dataset[data]] = ++grouped[element.dataset[data]] || 1;
+                });
+
+                for (attr in grouped)
+                    valid = valid || grouped[attr] === 3;
+
+                return valid;
+            }
+
+        ,   celebrate: function ()
+            {
+                if (this.winner)
+                    this.marker.innerHTML = 'Congrats Player <b class="player-' + this.winner + '">' + this.winner + ' ☺</b>';
+                else
+                    this.marker.innerHTML = 'It\'s a tie <b>☹</b>';
 
                 return this;
             }
